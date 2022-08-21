42f946aae2fcaf381e0c56f25a940f0b771b33f9
Anthony Santo <tonymsanto@gmail.com>
2022-08-20 22:38:10 -0600

    Working through API

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,18 +1,33 @@
+var weatherDash = "http://api.weatherbit.io/v2.0/";
+var APIKey = "ae31887480a74349b553027bfaa5c134";
+var cityName = document.getElementById(cityName);
+var currentDate = document.getElementById (currentDate);
+var weatherIcon = document.getElementById (weatherIcon);
+var currentTemp = document.getElementById (currentTemp);
+var currentHumidity = document.getElementById (currentHumidity);
+var currentWindSpeed = document.getElementById (currentWindSpeed);
+var currentUVIndex = document.getElementById (currentUVIndex);
+
+showSearchBtn.addEventListener("click", showWeather)
+
+let nameOfCity = document.getElementById('searchBar').value;
+console.log(nameOfCity);
 
 
-function getAPI() {
-    var weather = "http://api.weatherbit.io/v2.0/";
-
-    fetch(weather)
+function getWeather (response) {
+    var weatherDash = 
+        "http://api.weatherbit.io/v2.0/forecast/daily?city=" +
+        nameOfCity +
+        "&units=I" +
+        "&days=5" +
+        "&key=" +
+        APIKey;
+    fetch(weatherDash)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log (data);
-            for (var i = 0; i < data.length; i++) {
-                var userName = document.createElement('')
 
-            }
-            
         })
+
 }
